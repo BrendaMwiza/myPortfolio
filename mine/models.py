8ffb70159a8af6c166578c23dcfa28ae2be04892
BrendaMwiza <brendabrizy@gmail.com>
2019-11-13 09:39:40 +0200

    added urls

--- mine/models.py
+++ mine/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Bakery(models.Model):
+      name = models.CharField(max_length=32)
+      description = models.CharField(max_length=300)
+      post = models.ImageField(upload_to = 'pictures/')
+
+      def save_post(self):
+          self.save()
+
+      def __str__(self):
+        return self.name
--- mine/views.py
+++ mine/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+# from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Projects
+from .forms import OrderForm,BakeryForm
 
-# Create your views here.
+def home(request):
+    
+    images=Projects.objects.all()
+    return render(request,'index.html',{"images":images})
